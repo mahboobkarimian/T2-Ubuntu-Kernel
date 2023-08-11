7fa6534befc78ee2354ac99b6e29eaae46124783
Mahboob Karimian <63875592+mahboobkarimian@users.noreply.github.com>
2023-08-11 21:41:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,9 +26,11 @@ apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev
   libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves llvm zstd \
   wget rustc-1.62 rust-1.62-src rustfmt-1.62 bindgen-0.56 llvm clang
 
+set +e
 echo "DBG: Gettings info about latest tag"
 REMOTE_LATEST_TAG=$(git ls-remote --tags --sort=-creatordate ${KERNEL_REPOSITORY} 2>&1 | sed 's/.*refs\/tags\///' | sed 's/\^{}//')
 echo "DBG: Latest tag is ${REMOTE_LATEST_TAG}"
+set -e
 
 ### get Kernel
 git clone --depth 1 --single-branch --branch "${REMOTE_LATEST_TAG}" "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
