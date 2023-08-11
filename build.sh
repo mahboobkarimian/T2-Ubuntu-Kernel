a4068a904b7a3091dc7ca26438aeb91ef7cf2dd5
Mahboob Karimian <63875592+mahboobkarimian@users.noreply.github.com>
2023-08-11 21:27:50 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 set -eu -o pipefail
-
 KERNEL_REPOSITORY=git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/lunar
+echo "DBG: Gettings latest tag"
+REMOTE_LATEST_TAG=$(git ls-remote --tags --sort=-creatordate "${KERNEL_REPOSITORY}" 2>&1 | sed 's/.*refs\/tags\///' | sed 's/\^{}//')
+echo "DBG: Latest tag is "${REMOTE_LATEST_TAG}"
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 REPO_PATH=$(pwd)
 WORKING_PATH=/root/work
@@ -27,7 +29,6 @@ apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev
   libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves llvm zstd \
   wget rustc-1.62 rust-1.62-src rustfmt-1.62 bindgen-0.56 llvm clang
 
-REMOTE_LATEST_TAG=$(git ls-remote --tags --sort=-creatordate "${KERNEL_REPOSITORY}" 2>&1 | sed 's/.*refs\/tags\///' | sed 's/\^{}//')
 ### get Kernel
 git clone --depth 1 --single-branch --branch "${REMOTE_LATEST_TAG}" "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
 cd "${KERNEL_PATH}" || exit
