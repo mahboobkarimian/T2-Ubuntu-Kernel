a80d87bc718a30617ab7849c76d56855c5e22840
Mahboob Karimian <63875592+mahboobkarimian@users.noreply.github.com>
2023-08-12 18:59:02 +0200

    Compile > tag Ubuntu-6.2.0-27.27

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -eu -o pipefail
 
 KERNEL_REPOSITORY=https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/lunar
+echo "DBG: Gettings info about latest tag"
+REMOTE_LATEST_TAG=$(git ls-remote --tags $KERNEL_REPOSITORY | grep -v lowlatency | sort -k2 -t- -V | tail -1 | sed 's/.*refs\/tags\///' | sed 's/\^{}//')
+echo "DBG: Latest tag is ${REMOTE_LATEST_TAG}"
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 REPO_PATH=$(pwd)
 WORKING_PATH=/root/work
@@ -14,31 +17,17 @@ get_next_version () {
 ### Clean up
 rm -rfv ./*.deb
 
-mkdir "${WORKING_PATH}" && cd "${WORKING_PATH}"
-cp -rf "${REPO_PATH}"/{patches,templates} "${WORKING_PATH}"
+mkdir "$WORKING_PATH" && cd "${WORKING_PATH}"
 rm -rf "${KERNEL_PATH}"
 
-### Dependencies
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev libelf-dev \
-  openssl dkms libudev-dev libpci-dev libiberty-dev autoconf wget xz-utils git default-jdk \
-  libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves llvm zstd \
-  wget rustc-1.62 rust-1.62-src rustfmt-1.62 bindgen-0.56 llvm clang
-
-echo "DBG: Gettings info about latest tag"
-REMOTE_LATEST_TAG=$(git ls-remote --tags ${KERNEL_REPOSITORY} | grep -v lowlatency | sort -k2 -t- -V | tail -1 | sed 's/.*refs\/tags\///' | sed 's/\^{}//')
-echo "DBG: Latest tag is ${REMOTE_LATEST_TAG}"
 
 ### get Kernel
-git clone --depth 1 --single-branch --branch "${REMOTE_LATEST_TAG}" "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
+git clone --depth 1 --single-branch --branch "$REMOTE_LATEST_TAG" "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
 cd "${KERNEL_PATH}" || exit
-LATEST_TAG=$(git tag --sort=-creatordate | head -1)
-git checkout "$LATEST_TAG"
 
-KERNEL_VERSION="${LATEST_TAG}-generic"
+KERNEL_VERSION="${REMOTE_LATEST_TAG}-generic"
 
-IFS='-' read -r UBUNTU_NAME KERNEL_REL UBUNTU_REL <<< "$LATEST_TAG"
+IFS='-' read -r UBUNTU_NAME KERNEL_REL UBUNTU_REL <<< "$REMOTE_LATEST_TAG"
 
 ### Debug commands
 echo "$UBUNTU_NAME KERNEL_VERSION=$KERNEL_VERSION"
@@ -49,7 +38,7 @@ grep 'model name' /proc/cpuinfo | uniq
 
 #### Create patch file with custom drivers
 echo >&2 "===]> Info: Creating patch file... "
-KERNEL_VERSION="${KERNEL_VERSION}" WORKING_PATH="${WORKING_PATH}" "${REPO_PATH}/patch_driver.sh"
+KERNEL_VERSION="${KERNEL_VERSION}" WORKING_PATH="${WORKING_PATH}" /bin/bash "${REPO_PATH}/patch_driver.sh"
 
 #### Apply patches
 cd "${KERNEL_PATH}" || exit
@@ -66,18 +55,24 @@ while IFS= read -r file; do
   patch -p1 <"$file"
 done < <(find "${WORKING_PATH}/patches" -type f -name "*.patch" | sort)
 
+### Dependencies
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev libelf-dev \
+  openssl dkms libudev-dev libpci-dev libiberty-dev autoconf wget xz-utils git default-jdk \
+  libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves llvm zstd \
+  wget rustc-1.62 rust-1.62-src rustfmt-1.62 bindgen-0.56 llvm clang
+  
 chmod a+x "${KERNEL_PATH}"/debian/rules
 chmod a+x "${KERNEL_PATH}"/debian/scripts/*
 chmod a+x "${KERNEL_PATH}"/debian/scripts/misc/*
 
 cd "${KERNEL_PATH}"
-echo >&2 "===]> Info: Config kernel ... "
+echo >&2 "===]> Info: Get kernel config ... "
 wget https://raw.githubusercontent.com/mahboobkarimian/T2-Ubuntu-Kernel/Ubuntu/.config
-make olddefconfig
-# Build Deb packages
-echo >&2 "===]> Info: fakeroot clean... "
+
 sed -i "s/${KERNEL_REL}-${UBUNTU_REL}/${KERNEL_REL}-${UBUNTU_REL}+t2/g" debian.master/changelog
-#LANG=C fakeroot debian/rules clean
+
 # Disable debug info
 ./scripts/config --undefine GDB_SCRIPTS
 ./scripts/config --undefine DEBUG_INFO
@@ -93,19 +88,12 @@ make olddefconfig
 ./scripts/config --module CONFIG_HID_APPLE_IBRIDGE
 ./scripts/config --module CONFIG_HID_APPLE_TOUCHBAR
 ./scripts/config --module CONFIG_HID_APPLE_MAGIC_BACKLIGHT
-echo >&2 "===]> Info: Bulding src... "
-#make ARCH=x86 mrproper
-#LANG=C fakeroot debian/rules binary-headers binary-generic binary-perarch
-# Get rid of the dirty tag
-#echo "" >"${KERNEL_PATH}"/.scmversion
 
+echo >&2 "===]> Info: Building src... "
 # Build Deb packages
-make -j "$(getconf _NPROCESSORS_ONLN)" deb-pkg LOCALVERSION=-t2-"${CODENAME}" KDEB_PKGVERSION="${KERNEL_REL}-${UBUNTU_REL}"
+make -j "$(getconf _NPROCESSORS_ONLN)" deb-pkg LOCALVERSION=-t2-"${CODENAME}" KDEB_PKGVERSION="${KERNEL_REL}-${UBUNTU_REL}"-generic
 
 #### Copy artifacts to shared volume
 echo >&2 "===]> Info: Copying debs and calculating SHA256 ... "
-#cp -rfv ../*.deb "${REPO_PATH}/"
-#cp -rfv "${KERNEL_PATH}/.config" "${REPO_PATH}/kernel_config_${KERNEL_VERSION}"
-cp -rfv "${KERNEL_PATH}/debian/build/build-generic/.config" "/tmp/artifacts/kernel_config_${KERNEL_VERSION}"
 cp -rfv ../*.deb /tmp/artifacts/
 sha256sum ../*.deb >/tmp/artifacts/sha256
